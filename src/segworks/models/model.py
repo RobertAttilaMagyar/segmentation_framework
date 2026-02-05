5403c7946cc6fbf09cd1d6eeeaa06138e0f2c20c
RobertAttilaMagyar <robertmagyar.023@gmail.com>
2026-02-05 19:55:58 +0100

    base model pre commit test

--- src/segworks/models/model.py
+++ src/segworks/models/model.py
@@ -6,10 +6,10 @@ from torch import nn
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-    
-    def export(format: Literal['onnx']):
+
+    def export(format: Literal["onnx"]):
         match format:
             case 'onnx':
                 raise NotImplementedError()
             case _:
-                raise ValueError(f'Unsupported export format {format}')
+                raise ValueError(f"Unsupported export format {format}")

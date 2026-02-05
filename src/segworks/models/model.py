19c53c603e0d7a0af0c7f86625fd0a3a05b50e69
RobertAttilaMagyar <robertmagyar.023@gmail.com>
2026-02-05 19:58:55 +0100

    base model pre commit test

--- src/segworks/models/model.py
+++ src/segworks/models/model.py
@@ -7,12 +7,9 @@ class Model(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def export(format: Literal["onnx"]):
+    def export(format: Literal['onnx']):
         match format:
             case "onnx":
-
-
-                
                 raise NotImplementedError()
             case _:
                 raise ValueError(f"Unsupported export format {format}")
